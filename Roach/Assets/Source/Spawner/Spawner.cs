cddcd8374cfaba18bf2ddf230700cbbecc228944
Dmitry Kurilov <kurilov6dv@gmail.com>
2023-05-16 17:10:52 +0300

    Add 2 spawner

---
+++ Roach/Assets/Source/Spawner/Spawner.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _fishPrefab;
+    [SerializeField] private Transform[] _spawnAreas;
+    [SerializeField] private float _secondsBetweenSpawn;
+    [SerializeField] private float _spawnRange;
+
+    private float _elapsedTime = 0f;
+    
+    private void Update()
+    {
+        _elapsedTime += Time.deltaTime;
+        
+        if (_elapsedTime >= _secondsBetweenSpawn)
+        {
+            _elapsedTime = 0f;
+            CreateFishInArea();
+        }
+    }
+
+    private void CreateFishInArea()
+    {
+        int spawnPointNumber = Random.Range(0, _spawnAreas.Length);
+        Vector3 spawnPosition = GetRandomPosition(spawnPointNumber);
+
+        Instantiate(_fishPrefab, spawnPosition, Quaternion.identity);
+    }
+
+    private Vector3 GetRandomPosition(int spawnPointNumber)
+    {
+        Vector3 randomPosition = new Vector3(
+            _spawnAreas[spawnPointNumber].position.x, 
+            _spawnAreas[spawnPointNumber].position.y, 
+            Random.Range(_spawnAreas[spawnPointNumber].position.z - _spawnRange, _spawnAreas[spawnPointNumber].position.z + _spawnRange));
+
+        return randomPosition;
+    }
+}
